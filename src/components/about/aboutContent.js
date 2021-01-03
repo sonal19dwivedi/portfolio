2052a18238fcc9d711036b1a8a22409c96029326
sonal19dwivedi <55930375+sonal19dwivedi@users.noreply.github.com>
2021-01-03 13:40:08 -0500

    updated about and added footer

--- src/components/about/aboutContent.js
+++ src/components/about/aboutContent.js
@@ -1,9 +1,10 @@
 export const ABOUT_CONTENT = (
   <b>
-    Hi! <br /> I am <i>Sonal!</i> <br />
-    Experienced Developer with a demonstrated history of working in the information technology and services industry [SaaS]. Skilled in Java, Spring boot, MongoDB, PostgreSQL, React, JavaScript, TypeScript, Node. I am pursuing my <i>Master's in Computer Science</i> from the University of Ottawa. I
-    have recently completed my eight months co-op at <i>Kinaxis</i>. I have also worked as a <i>Teaching Assistant</i> for the courses Software Construction and Computer Architecture at the University of Ottawa. Prior to my master's, I was working as an <i>Assistant System Engineer</i> at the{" "}
-    <i>Tata Consultancy Services</i>.
+    I am a software developer with work experience in the information technology and services industry [SaaS]. Technologies with which I have worked
+    during my employment include Java, Spring boot, MongoDB, PostgreSQL, React, JavaScript, TypeScript, Electron, and Node. I will complete my
+    Master's in Computer Science from the University of Ottawa by April 2021. I recently completed an eight month co-op placement at Kinaxis. I have
+    also worked as a Teaching Assistant at the University of Ottawa, in Software Construction and Computer Architecture. Prior to my master's degree,
+    I was employed as an Assistant System Engineer at Tata Consultancy Services in Delhi, India.
   </b>
 );
 
--- src/components/nav-bar/bottomNav.jsx
+++ src/components/nav-bar/bottomNav.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import Copyright from "@material-ui/icons/Copyright";
 import GitHub from "@material-ui/icons/GitHub";
 import LinkedIn from "@material-ui/icons/LinkedIn";
 import Email from "@material-ui/icons/Email";
 import Snackbar from "@material-ui/core/Snackbar";
 import Slide from "@material-ui/core/Slide";
+import Typography from "@material-ui/core/Typography";
 import "./navBar.css";
 
 const EMAIL = "s19dwivedi@gmail.com";
@@ -46,13 +48,14 @@ export default function BottomNav() {
   return (
     <div className="footer sticky-footer">
       <BottomNavigation value={value} onChange={handleChange} className={classes.bottomNav}>
-        <BottomNavigationAction label="LinkedIn" value="LinkedIn" icon={<LinkedIn />} onClick={handleClick(openLinkedIn)}>
-          {console.log("clicked")}
-        </BottomNavigationAction>
+        <BottomNavigationAction label="LinkedIn" value="LinkedIn" icon={<LinkedIn />} onClick={handleClick(openLinkedIn)}></BottomNavigationAction>
         <BottomNavigationAction label="E-mail" value="E-mail" icon={<Email />} onClick={handleClick(popUpEmail)} />
         <BottomNavigationAction label="GitHub" value="GitHub" icon={<GitHub />} onClick={handleClick(openGitHub)} />
         <Snackbar open={open} onClose={handleClose} TransitionComponent={transition} message={EMAIL} key={transition ? transition.name : ""} />
       </BottomNavigation>
+      <Typography variant="body2" color="textSecondary" align="center">
+        Portfolio created using React, JavaScript, Node.js, HTML, Material UI, and CSS. Hosted on GitHub Pages. {<Copyright />} Sonal Dwivedi
+      </Typography>
     </div>
   );
 }
